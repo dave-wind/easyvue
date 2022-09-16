ef56e92c4bc9ac588fe4d6ad2edc6bdf5fa51c4a
dave-wind <xf.wei@ctrip.com>
2022-09-16 15:03:55 +0800

    feat(html):directive

--- test.js
+++ test.js
@@ -3,5 +3,9 @@ const compiler = require("vue-template-compiler");
 // r就是ast
 let r = compiler.compile(`<div style="color:red" id ="app"> hello {{name}}</div>`)
 
+// 指令
+let t = compiler.compile(`<div v-show="true">dave</div>`)
+
 
 console.log(r.render);
+console.log(t.render);
